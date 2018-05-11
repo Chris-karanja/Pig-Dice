1fece60a3862f6ba72878bb92d7f4543bb14e9c6
Chris-karanja <chriswainaina0@gmail.com>
2018-05-11 13:13:28 +0300

    finished writting the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-   total=0
-function Contact(first, total){
-   this.firstName = first;
-   this.total =  total;
+//biz logic
+var Player=function(name){
+  this,name=name
 }
+Player.prototype.rollDice = function () {
+  var total=0
+  var result=math.floor(math.random()*6)+1
+  if(dice < 2) {
+      var currentScore = 0;
+      return currentScore;
+    } else if(dice >= 2){
+      currentScore += dice
+      var i = 0;
+      return currentScore;
+    };
+  };
+};
