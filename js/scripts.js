0ea6046f410dad21eea7f141a7045a5c8c73b546
Chris-karanja <chriswainaina0@gmail.com>
2018-05-11 15:56:31 +0300

    added a few lines of js to the code

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,23 @@ Player.prototype.rollDice = function () {
       return currentScore;
     };
   };
-};
 
 //UI logic
 $(document).ready(function() {
-  $(#submit).click(function() {
+  $("#submit").click(function() {
     event.preventDefault();
 
-  })
-}
+      var playerName = $("input.player1").val();
+      var newPlayer = new player(playerName)
+        $("#name").text("<span class = 'name'>"+ playerName.name"</span>");
+
+  });
+$("#submit2").click(function() {
+  event.preventDefault();
+
+  var playerName = $("input.player2").val();
+  var newPlayer = new player(playerName)
+    $("#name").text("<span class = 'name'>"+ playerName.name"</span>");
+
+});
+  });
